593e17145e4fbd7c83d34641a649516dec19397e
Julio César Barbosa da Silva <j.barbosa.da.silva@avanade.com>
2024-06-23 13:46:18 -0300

    Fix: pypi e poetry

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='my_app_pypi',
-    version='0.1.2',
+    name='my_app_pypi2',
+    version='0.0.3',
     packages=find_packages(),
     install_requires=[
         'flask==2.0.1',
