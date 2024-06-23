56c1d5dfa53a2ed62353e53ede534e2b74c27242
Julio César Barbosa da Silva <j.barbosa.da.silva@avanade.com>
2024-06-23 13:18:51 -0300

    Fix

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='my_app__darvin2',
-    version='0.1.0',
+    name='my_app__py',
+    version='0.1.1',
     packages=find_packages(),
     install_requires=[
         'flask==2.0.1',
