9c9b16fcc4c4b08b4617918e9cb102230541cb34
Julio César Barbosa da Silva <j.barbosa.da.silva@avanade.com>
2024-06-24 17:30:43 -0300

    Fix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='my-app-pypi',
-    version='0.0.7',
+    version='0.0.8',
     packages=find_packages(),
     install_requires=[
         'flask==2.0.1',
